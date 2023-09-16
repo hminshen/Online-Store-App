eff26be3763a75ddebf16a0dcd8a237ef6f8cbe9
hminshen <hminshen@gmail.com>
2023-09-16 14:30:52 +0800

    Feature: Added body parser on backend to parse request body from frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,12 +4,22 @@ import express from 'express';
 import config from './config/endpoints.config';
 import corsOptions from './config/cors-config';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = config.PORT;
 
 // Middleware to parse incoming JSON data -- if not cannot parse req obj:
 app.use(express.json());
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
 
 app.use(
   cors(corsOptions)
