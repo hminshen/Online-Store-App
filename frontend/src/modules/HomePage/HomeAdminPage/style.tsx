d61013329df1d8e66bd221915e46c4f35056aed5
hminshen <hminshen@gmail.com>
2023-09-17 23:21:25 +0800

    Feature: Added signout button on admin manage page and signout api call, also improved styling on login page

---
+++ frontend/src/modules/HomePage/HomeAdminPage/style.tsx
@@ -0,0 +1,12 @@
+import { styled  } from '@mui/system';
+
+export const SignoutButton = styled('div')({
+    display: 'flex', 
+    justifyContent: 'center', 
+    marginBottom: '16px'
+  });
+
+export const CreateItemButton = styled('div')({
+    display: 'flex', 
+    justifyContent: 'flex-end'
+  });
