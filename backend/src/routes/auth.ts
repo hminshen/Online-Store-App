8a26f3a117bc7d473612dc6f85a0d96c3dcc4f6e
hminshen <hminshen@gmail.com>
2023-09-18 17:58:02 +0800

    Docs: Added API Docs for backend api routes documentation

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -6,17 +6,112 @@ import passport from 'passport';
 // Test route to get users
 router.get('/', authController.getUsers);
 
-// Sign Up route:
+/**
+ * @api {post} /signup Register a new user
+ * @apiName RegisterUser
+ * @apiGroup Authentication
+ *
+ * @apiParam (Request Body) {String} email User's email address. Must be unique.
+ * @apiParam (Request Body) {String} username User's username. Must be unique.
+ * @apiParam (Request Body) {String} password User's password. 
+ * @apiParam (Request Body) {String} name User's name.
+ *
+ * @apiSuccess {Object} user The newly registered user.
+ *
+ * @apiError {Object} error Error message if registration fails.
+ *
+ * @apiSuccessExample Success Response:
+ * HTTP/1.1 200 OK
+ * {
+ *   "user": {
+ *         "id": 2,
+ *         "email": "test2@gmail.com",
+ *         "username": "jack",
+ *         "name": "jack ray",
+ *         "hashed_password": {
+ *              "type": "Buffer",
+ *              "data": [
+ *                  14,
+ *                  100,
+ *                  ...
+ *              ]
+ *          },
+ *          "salt": {
+ *              "type": "Buffer",
+ *              "data": [
+ *                  89,
+ *                  ...
+ *              ]
+ *          },
+ *          "role_id": 1
+ *       }
+ * }
+ *
+ * @apiErrorExample Error Response:
+ * HTTP/1.1 500 Internal Server Error
+ * {
+ *   "error": "Could not create new user: Error message"
+ * }
+ */
 router.post('/signup', authController.createUser);
 
-// Login route:
+/**
+ * @api {post} /login Log in as a user
+ * @apiName LoginUser
+ * @apiGroup Authentication
+ * 
+ * @apiParam (Request Body) {String} username User's username.
+ * @apiParam (Request Body) {String} password User's password. 
+ *
+ * @apiSuccess {String} message Successful login message.
+ *
+ * @apiError {Object} error Error message if login fails.
+ *
+ * @apiSuccessExample Success Response:
+ * HTTP/1.1 200 OK
+ * {
+ *   "message": "Success"
+ * }
+ *
+ * @apiErrorExample Error Response:
+ * HTTP/1.1 500 Internal Server Error
+ * {
+ *   "error": "Login failed: Error message"
+ * }
+ */
+
 router.post('/login', passport.authenticate('local', {
     failureRedirect: '/login'
   }), (req, res) => {
     return res.status(200).json("success");
   });
 
-// Logout route:
+
+/**
+ * @api {get} /logout Log out a user
+ * @apiName LogoutUser
+ * @apiGroup Authentication
+ *
+ * @apiSuccess {String} message Successful logout message.
+ *
+ * @apiError {Object} error Error message if logout fails.
+ *
+ * @apiSuccessExample Success Response:
+ * HTTP/1.1 200 OK
+ * {
+ *   "message": "Logout successful"
+ * }
+ *
+ * @apiErrorExample Error Response:
+ * HTTP/1.1 400 Bad Request
+ * {
+ *   "error": "You are not logged in"
+ * }
+ * HTTP/1.1 500 Internal Server Error
+ * {
+ *   "error": "Logout failed: Error message"
+ * }
+ */
 router.get('/logout', (req, res) => {
     if (req.user === undefined){
         return res.status(400).json({ errormsg: "you are not logged in" });
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -4,11 +4,195 @@ import userController from '../controllers/user'
 import { checkIfLoggedInAPI, checkIfAdmin } from '../auth/checkAuth';
 import commonValidator from '../validators/commonValidator';
 
+/**
+ * @api {get} /users Get the current user
+ * @apiName GetCurrentUSer
+ * @apiGroup Users
+ *
+ * @apiDescription Get the current authenticated user.
+ *
+ * @apiSuccess (200) {Object} user The current user.
+ * @apiSuccess (200) {Number} user.id User's id.
+ * @apiSuccess (200) {String} user.username User's username.
+ * @apiSuccess (200) {Number} user.role_id User's role id.
+ *
+ * @apiSuccessExample {json} Success Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "id": 1,
+ *       "username": "exampleUser",
+ *       "role_id": "1"
+ *     }
+ *
+ * @apiError (401) Unauthorized. User not authenticated/logged in.
+ */
 router.get("/", checkIfLoggedInAPI, (req, res) => {
     res.status(200).json(req.user);
 });
+
+/**
+ * @api {get} /users/all Get all users
+ * @apiName GetUsers
+ * @apiGroup Users
+ *
+ * @apiDescription Get a list of all users.
+ *
+ * @apiSuccess (200) {Object[]} users List of users.
+ * @apiSuccess (200) {Number} user.id User's id.
+ * @apiSuccess (200) {String} user.email User's email.
+ * @apiSuccess (200) {String} user.username User's username.
+ * @apiSuccess (200) {String} user.name User's name.
+ * @apiSuccess (200) {Bytes} user.hashed_password User's Password (Hashed).
+ * @apiSuccess (200) {Bytes} user.salt User's Salt (Hashed).
+ * @apiSuccess (200) {Number} user.role_id User's role id.
+ *
+ * @apiSuccessExample {json} Success Response:
+ *     HTTP/1.1 200 OK
+ *     [
+ *       {
+ *          "id": 1,
+ *          "email": "test@gmail.com",
+ *          "username": "john",
+ *          "name": "john doe",
+ *          "hashed_password": {
+ *              "type": "Buffer",
+ *              "data": [
+ *                  11,
+ *                  225,
+ *                  ...
+ *              ]
+ *          },
+ *          "salt": {
+ *              "type": "Buffer",
+ *              "data": [
+ *                  113,
+ *                  ...
+ *              ]
+ *          },
+ *          "role_id": 2
+ *       },
+ *       {
+ *         "id": 2,
+ *         "email": "test2@gmail.com",
+ *         "username": "jack",
+ *         "name": "jack ray",
+ *         "hashed_password": {
+ *              "type": "Buffer",
+ *              "data": [
+ *                  14,
+ *                  100,
+ *                  ...
+ *              ]
+ *          },
+ *          "salt": {
+ *              "type": "Buffer",
+ *              "data": [
+ *                  89,
+ *                  ...
+ *              ]
+ *          },
+ *          "role_id": 1
+ *       },
+ *     ]
+ *
+ */
 router.get('/all', checkIfLoggedInAPI, checkIfAdmin, userController.getUsers);
+
+/**
+ * @api {get} /users/:id Get user by ID
+ * @apiName GetUserById
+ * @apiGroup Users
+ *
+ * @apiDescription Get a user by their ID.
+ *
+ * @apiParam {Number} id User's unique ID.
+ *
+ * @apiSuccess (200) {Object} user The requested user.
+ * @apiSuccess (200) {Number} user.id User's id.
+ * @apiSuccess (200) {String} user.email User's email.
+ * @apiSuccess (200) {String} user.username User's username.
+ * @apiSuccess (200) {String} user.name User's name.
+ * @apiSuccess (200) {Bytes} user.hashed_password User's Password (Hashed).
+ * @apiSuccess (200) {Bytes} user.salt User's Salt (Hashed).
+ * @apiSuccess (200) {Number} user.role_id User's role id.
+ *
+ * @apiSuccessExample {json} Success Response:
+ *     HTTP/1.1 200 OK
+ *       {
+ *         "id": 2,
+ *         "email": "test2@gmail.com",
+ *         "username": "jack",
+ *         "name": "jack ray",
+ *         "hashed_password": {
+ *              "type": "Buffer",
+ *              "data": [
+ *                  14,
+ *                  100,
+ *                  ...
+ *              ]
+ *          },
+ *          "salt": {
+ *              "type": "Buffer",
+ *              "data": [
+ *                  89,
+ *                  ...
+ *              ]
+ *          },
+ *          "role_id": 1
+ *       }
+ *
+ * @apiError (404) Not Found User not found.
+ */
 router.get('/:id', commonValidator.idValidation, userController.getUser);
+
+/**
+ * @api {post} /users Create User
+ * @apiName CreateUser
+ * @apiGroup Users
+ *
+ * @apiDescription Create a new user with the provided data.
+ *
+ * @apiParam (Request Body) {String} username User's username. Must be unique
+ * @apiParam (Request Body) {String} name User's name.
+ * @apiParam (Request Body) {String} email User's email. Must be unique
+ * @apiParam (Request Body) {String} password User's password.
+ *
+ * @apiSuccess (200) {Object} user The newly created user.
+ * @apiSuccess (200) {Number} user.id User's id.
+ * @apiSuccess (200) {String} user.email User's email.
+ * @apiSuccess (200) {String} user.username User's username.
+ * @apiSuccess (200) {String} user.name User's name.
+ * @apiSuccess (200) {Bytes} user.hashed_password User's Password (Hashed).
+ * @apiSuccess (200) {Bytes} user.salt User's Salt (Hashed).
+ * @apiSuccess (200) {Number} user.role_id User's role id.
+ *
+ * @apiSuccessExample {json} Success Response:
+ *     HTTP/1.1 200 OK
+ *       {
+ *         "id": 2,
+ *         "email": "test2@gmail.com",
+ *         "username": "jack",
+ *         "name": "jack ray",
+ *         "hashed_password": {
+ *              "type": "Buffer",
+ *              "data": [
+ *                  14,
+ *                  100,
+ *                  ...
+ *              ]
+ *          },
+ *          "salt": {
+ *              "type": "Buffer",
+ *              "data": [
+ *                  89,
+ *                  ...
+ *              ]
+ *          },
+ *          "role_id": 1
+ *       }
+ *
+ * @apiError (500) InternalServerError Could not create a new user.
+ */
 router.post('/', userController.createUser);
 
 export default router;
